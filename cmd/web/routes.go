7a3685a7c77cff9dbc8214a8f7d7d033c029ce2c
Navruzbek Rakhimov <rakhimovnavruzbek@gmail.com>
2021-07-08 23:38:43 +0500

    imported alice for managing middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+
+	// Using alice package for managing and chaining middlewares:
+	// creating a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -11,5 +20,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
