0ae06b45a962caf0c90aaeb0efe451145abbaf45
Navruzbek Rakhimov <rakhimovnavruzbek@gmail.com>
2021-07-12 18:52:46 +0500

    end-to-end testing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golangcollege/sessions"
+	"github.com/navruz-rakhimov/snippetbox/pkg/models"
 	"github.com/navruz-rakhimov/snippetbox/pkg/models/mysql"
 	"html/template"
 	"log"
@@ -20,10 +21,18 @@ const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
-	snippets *mysql.SnippetModel
+	snippets interface{
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
 	session *sessions.Session
-	users *mysql.UserModel
+	users interface{
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
